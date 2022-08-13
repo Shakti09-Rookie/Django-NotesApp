d16aeb1c363be41bb8e364e32b8e6dcef66215ed
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-08-13 23:23:30 +0530

    Initial Commit

---
+++ api/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import Note
+# from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import NoteSerializer
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Our API')
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all().order_by('-updated')
+    serializer = NoteSerializer(notes, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many = False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance = note, data = data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
